592ffcfb063101e58595178251b836a38469a4f0
TaoMaru <jackson_maria1@columbusstate.edu>
2023-02-21 22:41:27 -0500

    Add project files.

---
+++ frmComic.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picComic = New System.Windows.Forms.PictureBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblGroupSize = New System.Windows.Forms.Label()
+        Me.txtGroup = New System.Windows.Forms.TextBox()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.grpType = New System.Windows.Forms.GroupBox()
+        CType(Me.picComic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpType.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picComic
+        '
+        Me.picComic.Location = New System.Drawing.Point(174, 35)
+        Me.picComic.Name = "picComic"
+        Me.picComic.Size = New System.Drawing.Size(100, 50)
+        Me.picComic.TabIndex = 0
+        Me.picComic.TabStop = False
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Location = New System.Drawing.Point(222, 122)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(51, 17)
+        Me.lblHeading.TabIndex = 1
+        Me.lblHeading.Text = "Label1"
+        '
+        'lblGroupSize
+        '
+        Me.lblGroupSize.AutoSize = True
+        Me.lblGroupSize.Location = New System.Drawing.Point(225, 157)
+        Me.lblGroupSize.Name = "lblGroupSize"
+        Me.lblGroupSize.Size = New System.Drawing.Size(51, 17)
+        Me.lblGroupSize.TabIndex = 2
+        Me.lblGroupSize.Text = "Label2"
+        '
+        'txtGroup
+        '
+        Me.txtGroup.Location = New System.Drawing.Point(568, 151)
+        Me.txtGroup.Name = "txtGroup"
+        Me.txtGroup.Size = New System.Drawing.Size(100, 22)
+        Me.txtGroup.TabIndex = 3
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Location = New System.Drawing.Point(202, 350)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(51, 17)
+        Me.lblCost.TabIndex = 4
+        Me.lblCost.Text = "Label3"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(565, 350)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(51, 17)
+        Me.lblPrice.TabIndex = 5
+        Me.lblPrice.Text = "Label4"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(205, 408)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 6
+        Me.btnCalc.Text = "Button1"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(568, 407)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Button2"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(56, 21)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(56, 48)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(56, 79)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton3.TabIndex = 11
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'grpType
+        '
+        Me.grpType.Controls.Add(Me.RadioButton1)
+        Me.grpType.Controls.Add(Me.RadioButton3)
+        Me.grpType.Controls.Add(Me.RadioButton2)
+        Me.grpType.Location = New System.Drawing.Point(290, 217)
+        Me.grpType.Name = "grpType"
+        Me.grpType.Size = New System.Drawing.Size(200, 100)
+        Me.grpType.TabIndex = 12
+        Me.grpType.TabStop = False
+        Me.grpType.Text = "GroupBox1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.grpType)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.txtGroup)
+        Me.Controls.Add(Me.lblGroupSize)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.picComic)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.picComic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpType.ResumeLayout(False)
+        Me.grpType.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picComic As PictureBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblGroupSize As Label
+    Friend WithEvents txtGroup As TextBox
+    Friend WithEvents lblCost As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents grpType As GroupBox
+End Class
